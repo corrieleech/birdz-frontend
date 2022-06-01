320374526262aa528ad57bbf2f2500c84d5ed6fe
Corrie <corrieleech@gmail.com>
2022-06-01 15:56:29 -0500

    updates checklist new view with checklist name vmodel

--- src/views/ChecklistNew.vue
+++ src/views/ChecklistNew.vue
@@ -2,33 +2,43 @@
 import "axios";
 import Datepicker from "@vuepic/vue-datepicker";
 import "@vuepic/vue-datepicker/dist/main.css";
-// import { ref } from "vue";
+import { ref } from "vue";
+import axios from "axios";
 
 export default {
   components: {
     Datepicker,
   },
+  setup() {
+    const userTimeEntry = ref(new Date());
+
+    return {
+      userTimeEntry,
+    };
+  },
   data: function () {
     return {
       message: "Bird New List!",
       birder: localStorage.getItem("birder"),
-      likelyBirds: [],
-      checklist: {},
-      picked: {},
-      time: {
-        year: "",
-        month: "",
-        day: "",
-        hour: "",
-        minute: "",
-      },
+      checklistName: "",
+      location: "L161180",
     };
   },
   created: function () {},
   methods: {
     createList() {
       console.log("List created!");
-      console.log(this.picked);
+      axios({
+        method: "post",
+        url: "/checklists/",
+        data: {
+          name: this.checklistName,
+          locId: this.location,
+          time: this.userTimeEntry,
+        },
+      }).then(function (response) {
+        console.log("success!!", response.data);
+      });
     },
   },
 };
@@ -37,9 +47,11 @@ export default {
 <template>
   <div class="checklist-new">
     <div>
-      <h2>Enter time</h2>
-      <datepicker v-model="picked" />
-      {{ picked }}
+      <h2>Name Your Checklist</h2>
+      <input v-model="checklistName" />
+      <h2>Enter Day And Time for Checklist</h2>
+      <Datepicker v-model="userTimeEntry" placeholder="Select Day and Time" :is24="false" />
+      {{ userTimeEntry }}
     </div>
     <button v-on:click="createList()">Create your list</button>
   </div>
